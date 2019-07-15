c1ee969321d5d468f2cde0f057893fd138c1180d
TillKok <Cty960904@163.com>
2019-07-15 17:52:39 +0800

    配置REM适配

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,25 @@
-module.exports = {
-  plugins: {
-    autoprefixer: {}
+/**
+* post-css是一个后处理器
+* 与预处理器 less sass 处理之后的css再进一步处理
+*该文件是post-css的配置文件
+*它有很多插件：
+* autoprefixer 用来处理样式兼容前缀用的
+* postcss-pxtorem
+*/
+const { sep } = require('path')
+module.exports = ({ file }) => {
+  // path.sep 用于获取操作系统的路径分隔符
+  const rootValue = file.dirname.indexOf(`node_modules${sep}vant`) !== -1
+    ? 37.5
+    : 75
+
+  return {
+    plugins: {
+      autoprefixer: {},
+      'postcss-pxtorem': {
+        rootValue,
+        propList: ['*']
+      }
+    }
   }
 }
