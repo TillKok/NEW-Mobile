99ac2ff06ab84e85d3dbbc3edb9131b8b97b5ffe
TillKok <Cty960904@163.com>
2019-07-15 20:35:33 +0800

    登录页面布局

--- src/main.js
+++ src/main.js
@@ -7,11 +7,18 @@ import store from './store'
  * 这个第三方包引入即可，他会自动根据页面宽度设置网页基准字体大小
  */
 import 'amfe-flexible'
-import {
-  Button
-} from 'vant'
+/**
+ * 后面使用 */
 
-Vue.use(Button)
+// import {
+//   Button
+// } from 'vant'
+
+// Vue.use(Button)
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,11 +1,51 @@
 <template>
-  <div>login 页面</div>
+  <div class="login-wrap">
+    <van-nav-bar title="首页"/>
+    <!-- 表单 -->
+    <form>
+      <van-cell-group>
+          <van-field
+              v-model="user.mobile"
+              required
+              clearable
+              label="手机号"
+              placeholder="请输入手机号"
+          />
+          <van-field
+              v-model="user.code"
+              label="验证码"
+              placeholder="请输入验证码"
+              required
+              clearable
+          />
+      </van-cell-group>
+
+      <div class="login-btn-box">
+        <van-button class="login-btn" type="info">登录</van-button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'LoginIndex'
+  name: 'LoginIndex',
+  data () {
+    return {
+      user: {
+        mobile: '',
+        code: ''
+      }
+    }
+  }
 }
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.login-btn-box {
+    padding: 20px;
+    .login-btn {
+      width: 100%;
+    }
+}
+</style>
